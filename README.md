adfbc344bd6ecb8ab1bdf60694c39c949aa1a8af
kylinOS <weilong.ye.2012@gmail.com>
2016-03-23 21:39:40 -0300

    add install.sh

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MyVim
+wget -q0- https://github.com/WeilongYE/MyVim/master/install.sh | sh -x
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep
+cd ~/
+git clone https://github.com/vim/vim.git vim74
+cd vim74
+./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config/ --enable-perlinterp --enable-gui=gtk2 --enable-cscope --prefix=/usr --enable-luainterp 
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
+cd ~/
+rm -rf ~/vim74
+mv ~/.vim ~/.vim_old
+mv ~/.vimrc ~/.vimrc
+git clone https://github.com/WeilongYE/MyVim.git 
+cp -r ~/MyVim/vim ~/.vim
+cp  ~/MyVim/vimrc ~/.vimrc
+cp -r ~/MyVim/indexer_files ~/.indexer_files
+cp -r ~/MyVim/indexer_files_tags ~/.indexer_files_tags
