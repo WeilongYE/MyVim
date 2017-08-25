f533579b79d2a2bd74ff0db00f190b800ac692a0
WeilongYE <weilong.ye.2012@gmail.com>
2017-08-25 11:13:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #install vim
-sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep ctags
+sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep exuberant-ctags
 cd ~/
 git clone https://github.com/vim/vim.git vim74
 cd vim74
