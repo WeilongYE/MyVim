421e7150dec3789946ee8b5f0db8c22039646609
WeilongYE <weilong.ye.2012@gmail.com>
2020-07-24 13:48:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep exuberant-ctags silversearcher-ag
 cd ~
 git clone https://github.com/WeilongYE/MyVim.git 
+cd ~/MyVim
+git checkout new
 cp -r ~/MyVim/vim ~/.vim
 cp  ~/MyVim/vimrc ~/.vimrc
