53d0537c2c453094bdac2547f545f097945e0128
Weilong Ye <weilong.ye@avidbots.com>
2020-07-24 13:27:10 -0400

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-wget -q0- https://github.com/WeilongYE/MyVim/blob/master/install.sh | sh -x
+wget -q0- https://github.com/WeilongYE/MyVim/blob/new/install.sh | sh -x
 
--- install.sh
+++ install.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 #install vim
 sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep exuberant-ctags silversearcher-ag
-cd ~/
-git clone https://github.com/vim/vim.git vim74
-cd vim74
-./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config/ --enable-perlinterp --enable-gui=gtk2 --enable-cscope --prefix=/usr --enable-luainterp 
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
-sudo make install
-cd ~/
-rm -rf ~/vim74
-mv ~/.vim ~/.vim_old
-mv ~/.vimrc ~/.vimrc
+cd ~
 git clone https://github.com/WeilongYE/MyVim.git 
 cp -r ~/MyVim/vim ~/.vim
 cp  ~/MyVim/vimrc ~/.vimrc
-cp -r ~/MyVim/indexer_files ~/.indexer_files
-cp -r ~/MyVim/indexer_files_tags ~/.indexer_files_tags
-
-#install ycm
-sudo apt-get install build-essential cmake
-cd ~/.vim/bundle
-git clone https://github.com/Valloric/YouCompleteMe.git
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer
-rm -rf .git
-rm -f .gitignore
