345d6d4b03106115b5277732cafd97bfbed3c113
WeilongYE <weilong.ye.2012@gmail.com>
2023-03-16 21:57:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #install vim
-sudo apt-get install python-dev python3-dev ruby-dev libX11-dev libgtk-3-dev ncurses-dev ack-grep exuberant-ctags silversearcher-ag
+sudo apt-get install ack-grep exuberant-ctags silversearcher-ag
 cd ~
 git clone https://github.com/WeilongYE/MyVim.git 
 cd ~/MyVim
